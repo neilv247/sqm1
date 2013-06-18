f2066ead4dad15f9ee062d51592d74c30c699ddc
Neil Valentine <neilv247@gmail.com>
2013-06-18 15:39:44 +0100

    use service to share data between controllers (partialResults)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,55 +2,63 @@
 
 /* Controllers */
 
-
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
-
 
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
 
-function MySchedules($scope, $routeParams, $http) {
+function MySchedules($scope, $routeParams, $http, partialResults) {
   $http.get('inspections/neilston_p1.json').success(function(data) {
     $scope.inspection = data;
+//    alert( 'INSP: ' + $scope.inspection.INSPECTION);
+    partialResults.setObject(data);
   });
 }
 
 //MySchedules.$inject = ['$scope', '$routeParams', '$http'];
 
-function MyGroups($scope, $routeParams, $http, $location) {
+function MyGroups($scope, $routeParams, $http, $location, partialResults) {
 
-  $http.get('inspections/neilston_p1.json').success(function(data) {
-    $scope.inspection = data;
-  });
+//  $http.get('inspections/neilston_p1.json').success(function(data) {
+//    $scope.inspection = data;
+//  });
+  $scope.inspection = partialResults.getObject();
   $scope.groupix = $location.search()['groupix'];
+  $scope.doTheBack = function() {
+    window.history.back();
+  };    
 }
 
 //MyGroups.$inject = ['$scope', '$routeParams', '$http', '$location'];
 
-function MyFaults($scope, $routeParams, $http, $location) {
+function MyFaults($scope, $routeParams, $http, $location, partialResults) {
 
-  $http.get('inspections/neilston_p1.json').success(function(data) {
-    $scope.inspection = data;
+//  $http.get('inspections/neilston_p1.json').success(function(data) {
+//    $scope.inspection = data;
     $scope.groupix = $location.search()['groupix'];
     $scope.faultix = $location.search()['faultix'];
-    $scope.currDetail = data.HEADER[$scope.groupix].GROUP[$scope.faultix];
-  });
+    //$scope.currDetail = data.HEADER[$scope.groupix].GROUP[$scope.faultix];
+    $scope.inspection = partialResults.getObject();
+    $scope.currDetail = $scope.inspection.HEADER[$scope.groupix].GROUP[$scope.faultix];
+//  });
+
   $scope.checks = [
     {check:'P', bg:'green'},
     {check:'F', bg:'red'},
     {check:'N', bg:'white'}
   ];
+
   $scope.setFault = function(){
-        alert( 'value1: ' + $scope.currDetail.DETAIL[0].VALUE + ', grpix: ' + $scope.groupix + ', faultix: ' + $scope.faultix);
-	alert( 'value2: ' + $scope.currDetail.DETAIL[1].VALUE);
-	alert( 'value3: ' + $scope.currDetail.DETAIL[2].VALUE);
-	$scope.inspection.HEADER[$scope.groupix].GROUP[$scope.faultix].DETAIL = $scope.currDetail.DETAIL;
-    };
+//    alert( 'value1: ' + $scope.currDetail.DETAIL[0].VALUE + ', grpix: ' + $scope.groupix + ', faultix: ' + $scope.faultix);
+//	  alert( 'title1: ' + $scope.currDetail.DETAIL[0].TITLE);
+	  $scope.inspection.HEADER[$scope.groupix].GROUP[$scope.faultix] = $scope.currDetail;
+  };
+
   $scope.getFaultColor = function(fcheck){
         //alert( 'fcheck: ' + fcheck);
-	var fcolor = "white";
+	var fcolor = "warning";
 	if (fcheck == "P") 
 	{ fcolor = "success"; }
 	else
@@ -59,6 +67,10 @@ function MyFaults($scope, $routeParams, $http, $location) {
 	//alert( 'fcolor: ' + fcolor);
 	return fcolor;
     };
+
+  $scope.doTheBack = function() {
+    window.history.back();
+  };    
 }
 
 //MyFaults.$inject = ['$scope', '$routeParams', '$http', '$location'];
--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,51 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+var app = angular.module('myps', []);
+
+app.service('partialResults', function() {
+    var inspection = {
+  "STATION" : "NEILSTON",
+  "STATIONID" : 258,
+  "PLATFORM" : "1",
+  "INSPECTION" : "PRIMARY",
+  "REGIME" : "SQR",
+  "FAULT_TYPE" : "REACTIVE",
+  "FAULT_CATEGORY" : "STATION",
+  "AUTHORITY" : "SPT",
+  "USERID" : "U140",
+  "INSPECTION_DATE" : "",
+  "TS" : "",
+  "WEATHER" : "",
+  "HEADER" : [{
+    "CONFIGID" : "177",
+    "TITLE" : "TICKET OFFICE",
+    "SCHEDULE" : "1",
+    "FAILPOINT" : "L",
+    "FAILTYPE" : "S",
+    "GROUP" : [{
+      "CONFIGID" : "178",
+      "TITLE" : "Null Group Created for Schedule: 1",
+      "GROUPS" : "0",
+      "DETAIL" : [{
+        "CONFIGID" : 297,
+        "TITLE" : "TICKET OFFICE CLOSED",
+        "SSCODE" : "1.3",
+        "EXTRA" : "N",
+        "VALUE" : "N"
+      }]
+      }]
+      }]
+};
+    
+    return {
+        setObject: function(value) {
+            inspection = value;
+        },
+        getObject: function() {
+            return inspection;
+        }
+    }
+});
+
